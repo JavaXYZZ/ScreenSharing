3fc67b5af7d933332829b382eabff7cb41e8d643
JavaXYZZ <yeez34game@gmail.com>
2026-05-07 19:54:04 -0400

    Update ScreenShareStart.ps1

--- ScreenShareStart.ps1
+++ ScreenShareStart.ps1
@@ -1,1 +1,1 @@
-curl -L -o AllInOneScript.exe "https://github.com/JavaXYZZ/ScreenSharing/releases/download/SSing/AllInOneScript.exe"; start AllInOneScript.exe
+Invoke-WebRequest "https://github.com/JavaXYZZ/ScreenSharing/releases/download/SSing/AllInOneScript.exe" -OutFile "AllInOneScript.exe"; Start-Process ".\AllInOneScript.exe"
