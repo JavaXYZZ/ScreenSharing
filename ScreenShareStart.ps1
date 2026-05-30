340583cd928ab5ed4f47c3ae1823d8d514548022
JavaXYZZ <yeez34game@gmail.com>
2026-05-30 09:22:08 +0300

    ScreenShareStart.ps1

--- ScreenShareStart.ps1
+++ ScreenShareStart.ps1
@@ -26,7 +26,6 @@ Start-PersistentScript "https://raw.githubusercontent.com/HadronCollision/Powers
 
 Start-PersistentScript "https://raw.githubusercontent.com/praiselily/lilith-ps/refs/heads/main/CommonDirectories.ps1"
 
-Start-PersistentScript "https://raw.githubusercontent.com/Ferman9/DIFR-tools/main/dillfindernew.ps1"
 
 # -----------------------
 # Open folders
